38bdaedc1c75d559d633bbd2d15e194e4693b07f
Tahasin-tech <tahasinrafi@gmail.com>
2021-11-14 23:44:28 +0600

    sec-commit

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const port =process.env.PORT||5000;
 //middleware
 app.use(cors())
 app.use(express.json())
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnwfd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+// const uri = `mon//godb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnwfd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bl05f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 async function run(){
     try{
     await client.connect();
+    console.log('connected')
     const database=client.db('Travellia');
     const servicesCollection=database.collection('services')
     const orderCollection=database.collection('orders')
